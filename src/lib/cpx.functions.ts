99d6275ab110e8a58d21ec9748e01f0b522ac06c
Notagamyterxd <162592373+Notagamyterxd@users.noreply.github.com>
2026-09-04 18:46:06 +0200

    Update cpx.functions.ts

--- src/lib/cpx.functions.ts
+++ src/lib/cpx.functions.ts
@@ -1,13 +1,16 @@
-// Pure JavaScript MD5 Implementation (Requires zero external npm packages)
+// Fixed, Pure JavaScript MD5 Implementation (Requires zero external npm packages)
 function md5(string: string) {
   function k(n: number) { return Math.sin(n) * 4294967296 | 0; }
-  let b = [1732584193, -271733879, -1732584194, 271733878],
-      m = [],
-      s = [7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-           5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
-           4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-           6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21], i;
-  let str = unescape(encodeURIComponent(string)), l = str.length, n = (l + 8 >> 6) + 1, d = [];
+  let b = [1732584193, -271733879, -1732584194, 271733878];
+  let m: number[] = [];
+  let s = [
+    7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+    5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+    4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+    6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
+  ];
+  let i;
+  let str = unescape(encodeURIComponent(string)), l = str.length, n = (l + 8 >> 6) + 1, d: number[] = [];
   for (i = 0; i < l; ++i) d[i >> 2] |= str.charCodeAt(i) << (i % 4 << 3);
   d[l >> 2] |= 128 << (l % 4 << 3);
   d[n * 16 - 2] = l * 8;
@@ -15,8 +18,8 @@ function md5(string: string) {
     let a = b.slice(0), J = i * 16;
     for (let j = 0; j < 64; ++j) {
       let f, g;
-      if (j < 16) { f = a[1] & a[2] | ~a[1] & a[3]; g = j; }
-      else if (j < 32) { f = a[3] & a[1] | ~a[3] & a[2]; g = (5 * j + 1) % 16; }
+      if (j < 16) { f = (a[1] & a[2]) | (~a[1] & a[3]); g = j; }
+      else if (j < 32) { f = (a[1] & a[3]) | (a[2] & ~a[3]); g = (5 * j + 1) % 16; }
       else if (j < 48) { f = a[1] ^ a[2] ^ a[3]; g = (3 * j + 5) % 16; }
       else { f = a[2] ^ (a[1] | ~a[3]); g = (7 * j) % 16; }
       f = f + a[0] + (k(j + 1) >>> 0) + (d[J + g] >>> 0) | 0;
